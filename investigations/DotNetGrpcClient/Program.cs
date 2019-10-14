7c82eebe5f1f5145faab06bd20fe0a338356837c
Shashank Banerjea <shbanerj@microsoft.com>
2019-10-13 23:41:35 -0400

    Updated with python client and server

--- investigations/DotNetGrpcClient/Program.cs
+++ investigations/DotNetGrpcClient/Program.cs
@@ -11,13 +11,32 @@ namespace GrpcGreeterClient
         static async Task Main(string[] args)
         {
             // The port number(5001) must match the port of the gRPC server.
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            Console.WriteLine("Calling C# Endpoint...");
+            await CallEndpoint("http://localhost:5000", true);
+
+            Console.WriteLine("Calling Python Endpoint...");
+            await CallEndpoint("http://localhost:50051", true);
+            
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static GrpcChannel GetChannel(string grpcEndpointAddress, bool isInsecure = false)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", isInsecure);
+            return GrpcChannel.ForAddress(grpcEndpointAddress);           
+        }
+
+        static async Task CallEndpoint(string grpcEndpointAddress, bool isInsecure = false)
+        {
+            var channel = GetChannel(grpcEndpointAddress, isInsecure);
             var client = new Greeter.GreeterClient(channel);
+
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
         }
     }
 }
