c82229273309b82325b01f0f1dee10f4f4da6e34
Shawn Gaul <shgaul@microsoft.com>
2020-01-21 13:32:14 -0800

    Doc strings for Actions
    Call Activity and CallActivity with Retry

--- azure/durable_functions/models/actions/CallActivityAction.py
+++ azure/durable_functions/models/actions/CallActivityAction.py
@@ -5,6 +5,11 @@ from ..utils.json_utils import add_attrib
 
 
 class CallActivityAction:
+    """Defines the structure of the Call Activity object.
+
+    Provides the information needed by the durable extension to be able to schedule the activity.
+    """
+
     def __init__(self, function_name: str, input_=None):
         self.action_type: ActionType = ActionType.CALL_ACTIVITY
         self.function_name: str = function_name
@@ -14,6 +19,10 @@ class CallActivityAction:
             raise ValueError("function_name cannot be empty")
 
     def to_json(self) -> Dict[str, Any]:
+        """Convert object into a json dictionary.
+
+        :return: The instance of the class converted into a json dictionary
+        """
         json_dict = {}
         add_attrib(json_dict, self, 'action_type', 'actionType')
         add_attrib(json_dict, self, 'function_name', 'functionName')
