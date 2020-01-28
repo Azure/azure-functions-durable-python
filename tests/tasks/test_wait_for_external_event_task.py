5d1115b12310073628793f8d8faa328195834b2f
Shervyna Ruan <HsuanWei.Ruan@microsoft.com>
2020-01-28 12:53:22 +0900

    unittest for task_any, added tasks_test_utils

--- tests/tasks/test_wait_for_external_event_task.py
+++ tests/tasks/test_wait_for_external_event_task.py
@@ -1,25 +1,12 @@
-
-from tests.test_utils.ContextBuilder import ContextBuilder
 from azure.durable_functions.models.Task import Task
 from azure.durable_functions.tasks.wait_for_external_event import wait_for_external_event_task
 from azure.durable_functions.models.actions.WaitForExternalEventAction import WaitForExternalEventAction
 from tests.test_utils.constants import DATETIME_STRING_FORMAT
+from tests.test_utils.ContextBuilder import ContextBuilder
+from .tasks_test_utils import assert_tasks_equal
 from datetime import datetime
 import json
 
-
-def assert_actions_equal(action1, action2):
-    assert action1.action_type == action2.action_type
-    assert action1.external_event_name == action2.external_event_name   
-    assert action1.reason == action2.reason
-
-def assert_tasks_equal(task1, task2):
-    assert task1.is_completed == task2.is_completed
-    assert task1.is_faulted == task2.is_faulted
-    assert task1.result == task2.result
-    assert task1.timestamp == task2.timestamp
-    assert task1.id == task2.id
-    assert_actions_equal(task1.action, task2.action)
 
 def test_event_not_raised_return_incompleted_task():
     context_builder = ContextBuilder('test_simple_function')
