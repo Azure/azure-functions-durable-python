1ed87bab1297d88ba9fb7af77484d5521f3228e5
Shervyna Ruan <HsuanWei.Ruan@microsoft.com>
2020-01-29 14:19:53 +0900

    add handle faulted task_any case +unittest

--- azure/durable_functions/tasks/task_any.py
+++ azure/durable_functions/tasks/task_any.py
@@ -6,8 +6,6 @@ def task_any(tasks):
 
     Parameters
     ----------
-    state : List[HistoryEvent]
-        The list of history events.
     tasks : Task
         The tasks to evaluate their current state.
 
@@ -16,18 +14,24 @@ def task_any(tasks):
     TaskSet
         Returns a completed Durable Task Set if any of the tasks is completed.
         Returns a not completed Durable Task Set if none of the tasks are completed.
+        Returns a faulted Taskset if all tasks are faulted
     """
     all_actions = []
     completed_tasks = []
+    faulted_tasks = []
 
     for task in tasks:
         all_actions.append(task.action)
         if task.is_completed:
             completed_tasks.append(task)
+        if task.is_faulted:
+            faulted_tasks.append(task)
 
     completed_tasks.sort(key=lambda t: t.timestamp)
 
-    if len(completed_tasks) != 0:
+    if len(faulted_tasks) == len(tasks):
+        return TaskSet(False, all_actions, None, is_faulted=True)
+    elif len(completed_tasks) != 0:
         return TaskSet(True, all_actions, completed_tasks[0])
     else:
         return TaskSet(False, all_actions, None)
