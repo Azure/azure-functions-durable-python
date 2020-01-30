77ee6829639a95eda600f22f348e41ff8a5fcb5f
Shawn Gaul <shgaul@microsoft.com>
2020-01-29 16:52:10 -0800

    remove extra zimezone from format
    causing parsing warnings

--- azure/durable_functions/constants.py
+++ azure/durable_functions/constants.py
@@ -1,4 +1,4 @@
 """Constants used to determine the local running context."""
 DEFAULT_LOCAL_HOST: str = "localhost:7071"
 DEFAULT_LOCAL_ORIGIN: str = f"http://{DEFAULT_LOCAL_HOST}"
-DATETIME_STRING_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ%Z'
+DATETIME_STRING_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
