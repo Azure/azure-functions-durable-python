93145d4c3af656b6ae75c2b32aabc702d32ba818
Shervyna Ruan <HsuanWei.Ruan@microsoft.com>
2020-02-14 01:46:48 +0900

    update sample for create_check_status_response fixes

--- samples/external_events/DurableOrchestrationClient/__init__.py
+++ samples/external_events/DurableOrchestrationClient/__init__.py
@@ -8,5 +8,4 @@ async def main(req: func.HttpRequest, starter: str):
     client = DurableOrchestrationClient(starter)
     instance_id = await client.start_new(function_name, None, None)
 
-    response_args = client.create_check_status_response(req, instance_id)
-    return func.HttpResponse(**response_args)
+    return client.create_check_status_response(req, instance_id)
