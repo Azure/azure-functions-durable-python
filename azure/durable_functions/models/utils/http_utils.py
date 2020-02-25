45e5f286e5977f441368275cd35669618c888d63
Shawn Gaul <shgaul@microsoft.com>
2020-02-25 06:06:47 -0800

    add docstrings for new public functions

--- azure/durable_functions/models/utils/http_utils.py
+++ azure/durable_functions/models/utils/http_utils.py
@@ -5,6 +5,20 @@ import aiohttp
 
 @staticmethod
 async def post_async_request(url: str, data: Any = None) -> [int, Any]:
+    """Post request with the data provided to the url provided.
+
+    Parameters
+    ----------
+    url: str
+        url to make the post to
+    data: Any
+        object to post
+
+    Returns
+    -------
+    [int, Any]
+        Tuple with the Response status code and the data returned from the request
+    """
     async with aiohttp.ClientSession() as session:
         async with session.post(url,
                                 json=data) as response:
@@ -14,6 +28,18 @@ async def post_async_request(url: str, data: Any = None) -> [int, Any]:
 
 @staticmethod
 async def get_async_request(url: str) -> [int, Any]:
+    """Get the data from the url provided.
+
+    Parameters
+    ----------
+    url: str
+        url to get the data from
+
+    Returns
+    -------
+    [int, Any]
+        Tuple with the Response status code and the data returned from the request
+    """
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.json()
