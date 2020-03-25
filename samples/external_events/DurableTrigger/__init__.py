d01677a02bc095450db792654cfa031cc9c1b924
David Justo <david.justo.1996@gmail.com>
2020-03-25 15:01:57 -0700

    Correctness and documentation of the samples (#85)
    * validated all samples except tensorflow
    
    * deleted non-samples, tested tensorflow sample, added types and comments
    
    * new readmes for fan-out-fan-in and function-chaining
    
    * renamed some functions so they are easier to reason about
    
    * disable aiohttp's input validation... potentially unsafe. ExternalEvents now shows no warnings!

--- samples/external_events/DurableTrigger/__init__.py
+++ samples/external_events/DurableTrigger/__init__.py
@@ -4,7 +4,7 @@ from azure.durable_functions import DurableOrchestrationClient
 import azure.functions as func
 
 
-async def main(req: func.HttpRequest, starter: str, message):
+async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     """This function starts up the orchestrator from an HTTP endpoint
 
     Parameters
@@ -16,14 +16,18 @@ async def main(req: func.HttpRequest, starter: str, message):
     starter: str
         A JSON-formatted string describing the orchestration context
 
-    message:
-        An azure functions http output binding, it enables us to establish
-        an http response.
+    Returns
+    -------
+    func.HttpResponse
+        An HTTP response containing useful URLs for monitoring the
+        status of newly generated orchestration instance
     """
 
+    logging.debug("Recevied http request call with value {}".format(starter))
     function_name = req.route_params.get('functionName')
-    logging.info(starter)
     client = DurableOrchestrationClient(starter)
+
+    logging.debug("About to call function {} asyncrounously".format(function_name))
     instance_id = await client.start_new(function_name, None, None)
-    response = client.create_check_status_response(req, instance_id)
-    message.set(response)
+
+    return client.create_check_status_response(req, instance_id)
--- samples/external_events/RaiseEvent/__init__.py
+++ samples/external_events/RaiseEvent/__init__.py
@@ -6,6 +6,23 @@ import azure.functions as func
 
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
+    """Activity function to raise an external event to the orchestrator
+    
+    Parameters
+    ----------
+    req: func.HttpRequest
+        An HTTP Request object, it can be used to parse URL
+        parameters.
+
+    starter: str
+        A JSON-formatted string describing the orchestration context
+
+    Returns
+    -------
+    func.HttpResponse
+        HTTP response object whose body indicates which event
+        was raised
+    """
 
     logging.info("Recevied http request to check startus {}".format(starter))
     client = DurableOrchestrationClient(starter)
@@ -16,5 +33,4 @@ async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     logging.info("Will check on event: {}".format(event_name))
 
     await client.raise_event(instance_id, event_name, True)
-
     return func.HttpResponse(f'"{event_name}" event is sent')
