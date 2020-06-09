fe8ab32a7ffdd617f57284d7ea8d8e394c7f73f6
Priya Ananthasankar <prananth@microsoft.com>
2020-06-09 14:09:50 -0700

    aml monitoring

--- samples/aml_monitoring/aml_durable_orchestrator/__init__.py
+++ samples/aml_monitoring/aml_durable_orchestrator/__init__.py
@@ -3,16 +3,15 @@ import azure.durable_functions as df
 from datetime import datetime,timedelta
 
 def orchestrator_fn(context: df.DurableOrchestrationContext):
-    pipeline_endpoint = "https://westus2.aether.ms/api/v1.0/subscriptions/b2dffb01-098e-407e-b075-1a7c1bf68d2e/resourceGroups/mlops-sharing/providers/Microsoft.MachineLearningServices/workspaces/mlops-sharing/PipelineRuns/PipelineSubmit/bfe9fe7d-f480-40a3-a44f-58af185e06e5"
-    experiment_name = "fireside_chat_3"
+    pipeline_endpoint = ""
+    experiment_name = ""
 
     # Step 1:  Kickoff the AML pipeline
-    #input_args= {}
-    #input_args["pipeline_endpoint"] = pipeline_endpoint
-    #input_args["experiment_name"] = experiment_name
-    #input_args["params"] = None
-    #run_id = yield context.call_activity("aml_pipeline",input_args)
-    run_id = "ca1ffafb-0bea-4291-902b-548612a2ec49"
+    input_args= {}
+    input_args["pipeline_endpoint"] = pipeline_endpoint
+    input_args["experiment_name"] = experiment_name
+    input_args["params"] = None
+    run_id = yield context.call_activity("aml_pipeline",input_args)
     polling_interval = 60
     expiry_time = context.current_utc_datetime + timedelta(minutes=30)
 
