37794165908ef5e36e9d33b3bba28e8e79757280
David Justo <david.justo.1996@gmail.com>
2020-06-25 10:05:24 -0700

    more fine-grained handling of the exception

--- azure/durable_functions/models/DurableOrchestrationClient.py
+++ azure/durable_functions/models/DurableOrchestrationClient.py
@@ -73,10 +73,17 @@ class DurableOrchestrationClient:
 
         if response[0] <= 202 and response[1]:
             return response[1]["id"]
-        else:
-            # Surfacing the durable-extension exception
-            exception_message = response[1]
+        elif response[0] == 400:
+            # Orchestrator not found
+            exception_data = response[1]
+            exception_message = exception_data["ExceptionMessage"]
+            stack_trace = exception_data["StackTrace"]
+            exception_message += '\n' + stack_trace
             raise Exception(exception_message)
+        else:
+            # Catch all: simply surfacing the durable-extension exception
+            exception_message = response[1]
+            raise Exception(exception_message))
 
     def create_check_status_response(self, request, instance_id):
         """Create a HttpResponse that contains useful information for \
