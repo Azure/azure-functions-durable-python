8257e575547ac17235431ff89a36ac1098889dfa
David Justo <david.justo.1996@gmail.com>
2020-12-02 18:39:54 -0800

    docs typo

--- samples/durable_patterns/fan_in_fan_out/E2_GetFileList/__init__.py
+++ samples/durable_patterns/fan_in_fan_out/E2_GetFileList/__init__.py
@@ -5,7 +5,7 @@ def main(rootDirectory: str) -> List[str]:
 
     all_file_paths = []
     # We walk the file system, looking for all files
-    # below the current directory
+    # below "rootDirectory"
     for path, _, files in os.walk(rootDirectory):
         # For each file, we add their full-path to the list
         for name in files:
