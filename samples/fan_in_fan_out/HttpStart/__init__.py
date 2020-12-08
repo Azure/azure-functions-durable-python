42ba218b194bf7cf4f1de28eb42fc6fe7c4e68ff
David Justo <david.justo.1996@gmail.com>
2020-12-08 09:57:05 -0800

    Update samples

--- samples/fan_in_fan_out/HttpStart/__init__.py
+++ samples/fan_in_fan_out/HttpStart/__init__.py
@@ -1,12 +1,12 @@
 import logging
-
+import json
 import azure.functions as func
 import azure.durable_functions as df
 
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     client = df.DurableOrchestrationClient(starter)
-    payload = req.get_body()
+    payload: str = json.loads(req.get_body().decode())
     instance_id = await client.start_new(req.route_params["functionName"], client_input=payload)
 
     logging.info(f"Started orchestration with ID = '{instance_id}'.")
