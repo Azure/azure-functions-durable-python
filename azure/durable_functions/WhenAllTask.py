0a2787e6e52882a6f604857f53d1bd799ab3f98e
David Justo <david.justo.1996@gmail.com>
2021-06-07 17:00:35 -0700

    fourth pass

--- azure/durable_functions/WhenAllTask.py
+++ azure/durable_functions/WhenAllTask.py
@@ -1,2 +1,16 @@
 class WhenAllTask:
-    pass
+    def __init__(self, id, tasks):
+        self.pending_tasks = set()
+        self.completed_tasks = []
+     
+    def signal_completion(self, child):
+        try:
+            self.pending_tasks.remove(child)
+        except KeyError:
+            raise Exception("TBD")
+        
+        self.completed_tasks.append(child)
+    
+    @property
+    def is_completed(self):
+        return len(self.pending_tasks) == 0
--- azure/durable_functions/WhenAnyTask.py
+++ azure/durable_functions/WhenAnyTask.py
@@ -1,5 +1,13 @@
 class WhenAnyTask:
     def __init__(self, id, tasks):
-        self.
+        self.pending_tasks = set()
+        self.completed_tasks = set()
+        self.is_completed = len(self.completed_tasks) >= 1
      
-     def signal_completion(self, child):
+    def signal_completion(self, child):
+        try:
+            self.pending_tasks.remove(child)
+        except KeyError:
+            raise Exception("TBD")
+        
+        self.completed_tasks.add(child)
