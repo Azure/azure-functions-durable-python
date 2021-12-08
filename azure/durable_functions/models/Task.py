c450062df287d69a49ab230c533028546a485eeb
David Justo <david.justo.1996@gmail.com>
2021-12-08 14:09:13 -0800

    remove whitespace

--- azure/durable_functions/models/Task.py
+++ azure/durable_functions/models/Task.py
@@ -403,4 +403,3 @@ class RetryAbleTask(WhenAllTask):
             self.context._add_to_open_tasks(timer_task)
             self.is_waiting_on_timer = True
             self.error = child.result
-    
