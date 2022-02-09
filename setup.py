0dd70e0247662054314110870fd6f6d577d33d42
David Justo <david.justo.1996@gmail.com>
2022-02-09 13:48:48 -0800

    include _manifest in whl

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 import shutil
 import subprocess
 import sys
-import glob
 
+from glob import glob
 from setuptools import setup, find_packages
 from distutils.command import build
 
@@ -65,6 +65,9 @@ setup(
         'pytest-asyncio==0.10.0'
     ],
     include_package_data=True,
+    data_files= [
+        ('_manifest',glob('_manifest/**/*', recursive=True)),
+    ],
     cmdclass={
         'build': BuildModule
     },
