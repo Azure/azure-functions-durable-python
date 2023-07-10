19fba1aed9d8c5a02283946e92e4c2003d1cca8c
David Justo <david.justo.1996@gmail.com>
2023-07-10 14:50:33 -0700

    add blueprint example (#443)

--- samples-v2/function_chaining/function_app.py
+++ samples-v2/function_chaining/function_app.py
@@ -6,7 +6,7 @@ myApp = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @myApp.route(route="orchestrators/{functionName}")
 @myApp.durable_client_input(client_name="client")
-async def HttpStart(req: func.HttpRequest, client):
+async def http_start(req: func.HttpRequest, client):
     function_name = req.route_params.get('functionName')
     instance_id = await client.start_new(function_name)
     
@@ -14,12 +14,12 @@ async def HttpStart(req: func.HttpRequest, client):
     return client.create_check_status_response(req, instance_id)
 
 @myApp.orchestration_trigger(context_name="context")
-def E1_SayHello(context: df.DurableOrchestrationContext):
+def my_orchestrator(context: df.DurableOrchestrationContext):
     result1 = yield context.call_activity('say_hello', "Tokyo")
     result2 = yield context.call_activity('say_hello', "Seattle")
     result3 = yield context.call_activity('say_hello', "London")
     return [result1, result2, result3]
 
 @myApp.activity_trigger(input_name="city")
-def E1_SayHello(city: str) -> str:
+def say_hello(city: str) -> str:
     return f"Hello {city}!"
