a9dd0e0aaabbf6959457bde967113d71a38f2f88
Andy Staples <andystaples@microsoft.com>
2025-05-02 13:04:52 -0600

    Add tests to other projects

--- samples-v2/function_chaining/tests/test_my_orchestrator.py
+++ samples-v2/function_chaining/tests/test_my_orchestrator.py
@@ -1,7 +1,8 @@
+from datetime import timedelta
 import unittest
 from unittest.mock import Mock, call, patch
 
-from function_app import E2_BackupSiteContent
+from function_app import my_orchestrator
 
 # A way to wrap an orchestrator generator to simplify calling it and getting the results.
 # Because orchestrators in Durable Functions always accept the result of the previous activity for the next send() call, 
@@ -30,34 +31,28 @@ class MockTask():
 
 
 def mock_activity(activity_name, input):
-  if activity_name == "E2_GetFileList":
-    return MockTask(["C:/test/E2_Activity.py", "C:/test/E2_Orchestrator.py"])
-  return MockTask(input)
+  if activity_name == "say_hello":
+    return MockTask(f"Hello {input}!")
+  raise Exception("Activity not found")
 
 
 class TestFunction(unittest.TestCase):
   @patch('azure.durable_functions.DurableOrchestrationContext')
-  def test_E2_BackupSiteContent(self, context):
+  def test_chaining_orchestrator(self, context):
     # Get the original method definition as seen in the function_app.py file
-    func_call = E2_BackupSiteContent.build().get_user_function().orchestrator_function
+    func_call = my_orchestrator.build().get_user_function().orchestrator_function
 
-    context.get_input = Mock(return_value="C:/test")
     context.call_activity = Mock(side_effect=mock_activity)
-    context.task_all = Mock(return_value=MockTask([100, 200, 300]))
-
     # Create a generator using the method and mocked context
     user_orchestrator = func_call(context)
 
     # Use a method defined above to get the values from the generator. Quick unwrap for easy access
     values = [val for val in orchestrator_generator_wrapper(user_orchestrator)]
 
-    expected_activity_calls = [call('E2_GetFileList', 'C:/test'),
-                               call('E2_CopyFileToBlob', 'C:/test/E2_Activity.py'),
-                               call('E2_CopyFileToBlob', 'C:/test/E2_Orchestrator.py')]
+    expected_activity_calls = [call('say_hello', 'Tokyo'),
+                               call('say_hello', 'Seattle'),
+                               call('say_hello', 'London')]
     
     self.assertEqual(context.call_activity.call_count, 3)
     self.assertEqual(context.call_activity.call_args_list, expected_activity_calls)
-
-    context.task_all.assert_called_once()
-    # Sums the result of task_all
-    self.assertEqual(values[2], 600)
+    self.assertEqual(values[3], ["Hello Tokyo!", "Hello Seattle!", "Hello London!"])
