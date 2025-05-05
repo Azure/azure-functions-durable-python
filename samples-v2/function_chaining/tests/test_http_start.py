c5f3540fb7a39d34d5c759ada08b7d4e47d4c82a
Andy Staples <andystaples@microsoft.com>
2025-05-05 13:38:05 -0600

    Fix tests

--- samples-v2/function_chaining/tests/test_http_start.py
+++ samples-v2/function_chaining/tests/test_http_start.py
@@ -14,7 +14,7 @@ class TestFunction(unittest.TestCase):
     req = func.HttpRequest(method='GET',
                            body=b'{}',
                            url='/api/my_second_function',
-                           route_params={"functionName": "E2_BackupSiteContent"})
+                           route_params={"functionName": "my_orchestrator"})
 
     client.start_new = AsyncMock(return_value="instance_id")
     client.create_check_status_response = Mock(return_value="check_status_response")
@@ -22,6 +22,6 @@ class TestFunction(unittest.TestCase):
     # Create a generator using the method and mocked context
     result = asyncio.run(func_call(req, client))
 
-    client.start_new.assert_called_once_with("E2_BackupSiteContent", client_input={})
+    client.start_new.assert_called_once_with("my_orchestrator", client_input={})
     client.create_check_status_response.assert_called_once_with(req, "instance_id")
     self.assertEqual(result, "check_status_response")
