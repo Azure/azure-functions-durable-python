4eb1a80762d2573c768b8525bc6f1d569341817a
Andy Staples <andystaples@microsoft.com>
2025-06-11 13:44:58 -0600

    Python to 3.9 everywhere

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
-@nox.session(python=["3.7","3.8"])
+@nox.session(python=["3.9","3.11"])
 def tests(session):
     # same as pip install -r -requirements.txt
     session.install("-r", "requirements.txt")
@@ -8,19 +8,19 @@ def tests(session):
     session.run("pytest", "-v", "tests")
 
 
-@nox.session(python=["3.7", "3.8"])
+@nox.session(python=["3.9","3.11"])
 def lint(session):
     session.install("flake8")
     session.install("flake8-docstrings")
     session.run("flake8", "./azure/")
 
-@nox.session(python=["3.7", "3.8"])
+@nox.session(python=["3.9","3.11"])
 def typecheck(session):
     session.install("-r", "requirements.txt")
     session.install("mypy")
     session.run("mypy", "./azure/")
 
-@nox.session(python=["3.7", "3.8"])
+@nox.session(python=["3.9","3.11"])
 def autopep(session):
     session.install("-r", "requirements.txt")
     session.run("autopep8", "--in-place --aggressive --aggressive --recursive \"./azure/\"")
